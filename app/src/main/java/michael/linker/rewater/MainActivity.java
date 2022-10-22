6a05a558068222a9c436fba2b7f5f4077891698e
Mikhail Linker <work.michaellinker@gmail.com>
2022-10-22 22:54:35 +0300

    #6 Custom toolbar has been designed

--- app/src/main/java/michael/linker/rewater/MainActivity.java
+++ app/src/main/java/michael/linker/rewater/MainActivity.java
@@ -1,8 +1,8 @@
 package michael.linker.rewater;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
