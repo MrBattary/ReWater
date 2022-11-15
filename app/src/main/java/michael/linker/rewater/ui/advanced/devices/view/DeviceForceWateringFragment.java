1e97f114c9f844bfda8693992e4d0dd5b40746e2
Mikhail Linker <work.michaellinker@gmail.com>
2022-11-15 21:55:10 +0300

    #79 Added base devices info and force watering fragment

---
+++ app/src/main/java/michael/linker/rewater/ui/advanced/devices/view/DeviceForceWateringFragment.java
@@ -0,0 +1,40 @@
+package michael.linker.rewater.ui.advanced.devices.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import michael.linker.rewater.R;
+import michael.linker.rewater.ui.advanced.devices.viewmodel.DevicesViewModel;
+
+public class DeviceForceWateringFragment extends Fragment {
+    private DevicesViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+
+        NavController navController = NavHostFragment.findNavController(this);
+        ViewModelStoreOwner viewModelStoreOwner = navController.getViewModelStoreOwner(
+                R.id.root_navigation_devices);
+        mViewModel = new ViewModelProvider(viewModelStoreOwner).get(DevicesViewModel.class);
+
+        return inflater.inflate(R.layout.fragment_devices_force_watering, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // TODO: Use the ViewModel
+    }
+
+}
