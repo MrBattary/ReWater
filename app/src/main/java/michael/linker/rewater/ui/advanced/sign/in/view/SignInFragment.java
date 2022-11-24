f2f411c7ca574ae7aae6485c07d509c4c229896c
Mikhail Linker <work.michaellinker@gmail.com>
2022-11-24 22:57:53 +0300

    #114 Added navigation between sign in

--- app/src/main/java/michael/linker/rewater/ui/advanced/sign/in/view/SignInFragment.java
+++ app/src/main/java/michael/linker/rewater/ui/advanced/sign/in/view/SignInFragment.java
@@ -9,29 +9,50 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.button.MaterialButton;
 
 import michael.linker.rewater.R;
 import michael.linker.rewater.ui.advanced.sign.in.viewmodel.SignInViewModel;
 
 public class SignInFragment extends Fragment {
+    private MaterialButton mSignInButton, mSignUpSwitchButton, mRestorePasswordButton;
 
     private SignInViewModel mViewModel;
-
-    public static SignInFragment newInstance() {
-        return new SignInFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
+        NavController navController = NavHostFragment.findNavController(this);
+        ViewModelStoreOwner viewModelStoreOwner = navController.getViewModelStoreOwner(
+                R.id.navigation_sign);
+        mViewModel = new ViewModelProvider(viewModelStoreOwner).get(SignInViewModel.class);
+
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SignInViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.initFields(view);
+        this.initButtons(view);
+    }
+
+    private void initFields(final View view) {
+        mSignInButton = view.findViewById(R.id.sign_in_sign_in_button);
+        mSignUpSwitchButton = view.findViewById(R.id.sign_in_sign_up_button);
+    }
+
+    private void initButtons(final View view) {
+        final NavController navController = Navigation.findNavController(view);
+
+        mSignUpSwitchButton.setOnClickListener(
+                l -> navController.navigate(R.id.navigation_action_sign_in_to_sign_up));
     }
 
 }
--- app/src/main/java/michael/linker/rewater/ui/advanced/sign/up/view/SignUpFragment.java
+++ app/src/main/java/michael/linker/rewater/ui/advanced/sign/up/view/SignUpFragment.java
@@ -9,29 +9,49 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.button.MaterialButton;
 
 import michael.linker.rewater.R;
 import michael.linker.rewater.ui.advanced.sign.up.viewmodel.SignUpViewModel;
 
 public class SignUpFragment extends Fragment {
+    private MaterialButton mSignInSwitchButton, mSignUpButton;
 
     private SignUpViewModel mViewModel;
-
-    public static SignUpFragment newInstance() {
-        return new SignUpFragment();
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
+        NavController navController = NavHostFragment.findNavController(this);
+        ViewModelStoreOwner viewModelStoreOwner = navController.getViewModelStoreOwner(
+                R.id.navigation_sign);
+        mViewModel = new ViewModelProvider(viewModelStoreOwner).get(SignUpViewModel.class);
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.initFields(view);
+        this.initButtons(view);
+    }
+
+    private void initFields(final View view) {
+        mSignInSwitchButton = view.findViewById(R.id.sign_up_sign_in_button);
+        mSignUpButton = view.findViewById(R.id.sign_up_sign_up_button);
+    }
+
+    private void initButtons(final View view) {
+        final NavController navController = Navigation.findNavController(view);
+
+        mSignInSwitchButton.setOnClickListener(
+                l -> navController.navigate(R.id.navigation_action_sign_up_to_sign_in));
     }
 
 }
