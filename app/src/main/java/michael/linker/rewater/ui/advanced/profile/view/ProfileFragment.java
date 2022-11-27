9c1fa439da4b9a2e76bd372961a46474d835ede2
Mikhail Linker <work.michaellinker@gmail.com>
2022-11-27 18:46:47 +0300

    #125 Added profile and home fragment layouts

--- app/src/main/java/michael/linker/rewater/ui/advanced/profile/view/ProfileFragment.java
+++ app/src/main/java/michael/linker/rewater/ui/advanced/profile/view/ProfileFragment.java
@@ -24,7 +24,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
