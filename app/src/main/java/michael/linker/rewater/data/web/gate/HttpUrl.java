886f5eb01beaab3d3c31775825a5f67e6c158a09
Mikhail Linker <work.michaellinker@gmail.com>
2022-12-09 14:20:22 +0300

    #155 Updated backslashes positions in the url

--- app/src/main/java/michael/linker/rewater/data/web/gate/HttpUrl.java
+++ app/src/main/java/michael/linker/rewater/data/web/gate/HttpUrl.java
@@ -14,8 +14,8 @@ public class HttpUrl {
         public static final Protocol HTTP;
         public static final Protocol HTTPS;
 
-        private static final String HTTP_PROTOCOL = "http://";
-        private static final String HTTPS_PROTOCOL = "https://";
+        private static final String HTTP_PROTOCOL = "http:/";
+        private static final String HTTPS_PROTOCOL = "https:/";
 
         static {
             HTTP = new Protocol(HTTP_PROTOCOL);
@@ -50,10 +50,10 @@ public class HttpUrl {
         public static final Core GOOGLE;
         public static Core CORE;
 
-        private static final String GOOGLE_URL = "google.com/";
-        public static final String ANDROID_LOCAL_URL = "10.0.2.2/";
-        public static final String LOCAL_CORE_URL = "rewater.local/";
-        public static final String API_URL = "api/";
+        private static final String GOOGLE_URL = "/google.com";
+        public static final String ANDROID_LOCAL_URL = "/10.0.2.2";
+        public static final String LOCAL_CORE_URL = "/rewater.local";
+        public static final String API_URL = "/api";
 
         static {
             GOOGLE = new Core(GOOGLE_URL);
@@ -113,9 +113,9 @@ public class HttpUrl {
         public static final Group SCHEDULES;
         public static final Group DEVICES;
 
-        private static final String NETWORKS_GROUP = "networks/";
-        private static final String SCHEDULES_GROUP = "schedules/";
-        private static final String DEVICES_GROUP = "devices/";
+        private static final String NETWORKS_GROUP = "/networks";
+        private static final String SCHEDULES_GROUP = "/schedules";
+        private static final String DEVICES_GROUP = "/devices";
 
         static {
             NETWORKS = new Group(NETWORKS_GROUP);
